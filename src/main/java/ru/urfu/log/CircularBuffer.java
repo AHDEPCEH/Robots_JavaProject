e61b122c18aec9083053c083c8f837e1b3e8a2c2
AHDEPCEH <twunk.ctatucta@gmail.com>
2024-05-20 20:49:31 +0500

    изменил реализацию структуры хранения логов

--- src/main/java/ru/urfu/log/CircularBuffer.java
+++ src/main/java/ru/urfu/log/CircularBuffer.java
@@ -1,20 +1,85 @@
 package ru.urfu.log;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Кольцевой буфер ограниченного размера для хранения последних n добавленных объектов
+ * Итерируемый лист по записям из сегмента
+ * @param <T>
+ */
+class myList<T> implements Iterable<T> {
+    private Segment<T> currentSegment;
+    private final int lenSegment;
+    private int current;
+    private final int startIndex;
+    private final int endIndex;
+    private int count = 0;
+
+    /**
+     * Конструктор класса
+     * @param segment - сегмент с которого начинается итерация
+     * @param offset - сдвиг по количеству записей от начала
+     * @param startIndex - начало записей
+     * @param endIndex - конец записей
+     * @param lenSegment - размер сегмента
+     */
+    public myList(Segment<T> segment, int offset, int startIndex, int endIndex, int lenSegment) {
+        currentSegment = segment;
+        current = startIndex + offset;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.lenSegment = lenSegment;
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return count < endIndex - startIndex;
+            }
+
+            @Override
+            public T next() {
+                if (current >= lenSegment) {
+                    for (int i = 0; i < current / lenSegment; i++) {
+                        currentSegment = currentSegment.next;
+                    }
+                    current = current % lenSegment;
+                }
+                T element = currentSegment.entries.get(current);
+                current++;
+                count++;
+                return element;
+            }
+        };
+    }
+}
+
+/**
+ * Сегмент содержащий несколько записей
+ * @param <T> - тип хранимой записи
+ */
+class Segment<T>{
+    public Segment<T> next;
+    public List<T> entries = new LinkedList<>();
+
+    public Segment(){}
+}
+
+/**
+ * Кольцевой буфер ограниченного размера для хранения последних n добавленных объектов.
+ * Состоит из нескольких сегментов, каждый из которых имеет ссылку на следующий сегмент.
  * @param <T> - Тип объекта
  */
 public class CircularBuffer<T> {
-    private final T[] buffer;
+    private Segment<T> tail;
+    private Segment<T> head;
+    private final int lenSegment;
+    private final int maxLen;
     private int size;
-    private int head;
-    private int tail;
     private final Lock lock = new ReentrantLock();
 
     /**
@@ -22,33 +87,37 @@ public class CircularBuffer<T> {
      * @param capacity - размер буфера
      */
     public CircularBuffer(int capacity) {
-        this.buffer = (T[]) new Object[capacity];
+        head = new Segment<>();
+        tail = head;
+        maxLen = capacity;
+        lenSegment = (int) Math.sqrt(capacity);
         this.size = 0;
-        this.head = 0;
-        this.tail = 0;
     }
 
     /**
      * @return Возвращает количество элементов в буфере
      */
     public int size() {
-        return size;
+        return Math.min(size, maxLen);
     }
 
     /**
      * Добавление элементов в буфер
-     * @param element
+     * @param element - элемент для добавления
      */
     public void add(T element) {
         lock.lock();
         try {
-            if (size == buffer.length) {
-                head = (head + 1) % buffer.length; // удаление старого элемента
-            } else {
-                size++;
+            if (size % lenSegment == 0 && size > 0) {
+                head.next = new Segment<>();
+                head = head.next;
+                if (size > maxLen) {
+                    tail = tail.next;
+                    size -= lenSegment;
+                }
             }
-            buffer[tail] = element;
-            tail = (tail + 1) % buffer.length;
+            size += 1;
+            head.entries.add(element);
         } finally {
             lock.unlock();
         }
@@ -63,28 +132,7 @@ public class CircularBuffer<T> {
     public Iterable<T> iterator(int startIndex, int endIndex) {
         lock.lock();
         try {
-            return new Iterable<>() {
-                private int current = startIndex;
-                private int count = 0;
-
-                @Override
-                public Iterator<T> iterator() {
-                    return new Iterator<T>() {
-                        @Override
-                        public boolean hasNext() {
-                            return count < endIndex - startIndex;
-                        }
-
-                        @Override
-                        public T next() {
-                            T element = buffer[current];
-                            current = (current + 1) % buffer.length;
-                            count++;
-                            return element;
-                        }
-                    };
-                }
-            };
+            return new myList<>(tail, Math.max(size - maxLen, 0), startIndex, endIndex, lenSegment);
         } finally {
             lock.unlock();
         }
